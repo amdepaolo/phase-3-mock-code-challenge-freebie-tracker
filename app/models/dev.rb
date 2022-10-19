308b012311c70b0f458950603deccca02a66f768
Adam DePaolo <amdepaolo@gmail.com>
2022-10-19 18:04:22 -0400

    commit tree 507bc200a933e33533f1025a9c3cd9685efbbc25

--- app/models/dev.rb
+++ app/models/dev.rb
@@ -1,3 +1,17 @@
 class Dev < ActiveRecord::Base
+    has_many :freebies
+    has_many :companies, through: :freebies
+
+    def received_one? (item_name)
+        !!self.freebies.find_by(item_name: item_name)
+    end
+
+    def give_away(dev, freebie)
+        if self.id == freebie.dev_id
+            freebie.update(dev_id: dev.id)
+        else
+            "Can't give away this freebie"
+        end
+    end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -11,6 +11,10 @@ Dev.create(name: "Mr. Meseeks")
 Dev.create(name: "Gazorpazop")
 
 puts "Creating freebies..."
+Freebie.create(item_name: "Portal Gun", value: 20, dev_id: 1, company_id: 1)
+Freebie.create(item_name: "Sandals", value: 20, dev_id: 2, company_id: 2)
+Freebie.create(item_name: "Pizza Hut Coupon", value: 20, dev_id: 3, company_id: 3)
+Freebie.create(item_name: "Gift Certificate", value: 20, dev_id: 2, company_id: 4)
 
 # ***************************************************************
 # * TODO: create freebies! Remember, a freebie belongs to a dev *
